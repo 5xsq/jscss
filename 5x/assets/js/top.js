65c5debffc6a3c1682d30c83ca99bb328931c09a
5xsq <152060845+5xsq@users.noreply.github.com>
2024-06-24 16:11:39 +0800

    Add files via upload

---
+++ 5x/assets/js/top.js
@@ -0,0 +1,57 @@
+var scrolltotop={
+	setting:{
+		startline:100, //起始行
+		scrollto:0, //滚动到指定位置
+		scrollduration:400, //滚动过渡时间
+		fadeduration:[500,100] //淡出淡现消失
+	},
+	controlHTML:'<img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNy4xLjAsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTZweCIgaGVpZ2h0PSIxNnB4IiB2aWV3Qm94PSIwIDAgMTYgMTYiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDE2IDE2IiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxwb2x5Z29uIGZpbGw9IiNGRkZGRkYiIHBvaW50cz0iOCwyLjggMTYsMTAuNyAxMy42LDEzLjEgOC4xLDcuNiAyLjUsMTMuMiAwLDEwLjcgIi8+DQo8L3N2Zz4NCg==" style="width:40px; height:40px; border:0;background: rgb(255 0 85 / 63%);opacity: .5;" />', //返回顶部按钮
+	controlattrs:{offsetx:30,offsety:80},//返回按钮固定位置
+	anchorkeyword:"#top",
+	state:{
+		isvisible:false,
+		shouldvisible:false
+	},scrollup:function(){
+		if(!this.cssfixedsupport){
+			this.$control.css({opacity:0});
+		}
+		var dest=isNaN(this.setting.scrollto)?this.setting.scrollto:parseInt(this.setting.scrollto);
+		if(typeof dest=="string"&&jQuery("#"+dest).length==1){
+			dest=jQuery("#"+dest).offset().top;
+		}else{
+			dest=0;
+		}
+		this.$body.animate({scrollTop:dest},this.setting.scrollduration);
+	},keepfixed:function(){
+		var $window=jQuery(window);
+		var controlx=$window.scrollLeft()+$window.width()-this.$control.width()-this.controlattrs.offsetx;
+		var controly=$window.scrollTop()+$window.height()-this.$control.height()-this.controlattrs.offsety;
+		this.$control.css({left:controlx+"px",top:controly+"px"});
+	},togglecontrol:function(){
+		var scrolltop=jQuery(window).scrollTop();
+		if(!this.cssfixedsupport){
+			this.keepfixed();
+		}
+		this.state.shouldvisible=(scrolltop>=this.setting.startline)?true:false;
+		if(this.state.shouldvisible&&!this.state.isvisible){
+			this.$control.stop().animate({opacity:1},this.setting.fadeduration[0]);
+			this.state.isvisible=true;
+		}else{
+			if(this.state.shouldvisible==false&&this.state.isvisible){
+				this.$control.stop().animate({opacity:0},this.setting.fadeduration[1]);
+				this.state.isvisible=false;
+			}
+		}
+	},init:function(){
+		jQuery(document).ready(function($){
+			var mainobj=scrolltotop;
+			var iebrws=document.all;
+			mainobj.cssfixedsupport=!iebrws||iebrws&&document.compatMode=="CSS1Compat"&&window.XMLHttpRequest;
+			mainobj.$body=(window.opera)?(document.compatMode=="CSS1Compat"?$("html"):$("body")):$("html,body");
+			mainobj.$control=$('<div id="topcontrol">'+mainobj.controlHTML+"</div>").css({position:mainobj.cssfixedsupport?"fixed":"absolute",bottom:mainobj.controlattrs.offsety,right:mainobj.controlattrs.offsetx,opacity:0,cursor:"pointer"}).attr({title:"返回顶部"}).click(function(){mainobj.scrollup();return false;}).appendTo("body");if(document.all&&!window.XMLHttpRequest&&mainobj.$control.text()!=""){mainobj.$control.css({width:mainobj.$control.width()});}mainobj.togglecontrol();
+			$('a[href="'+mainobj.anchorkeyword+'"]').click(function(){mainobj.scrollup();return false;});
+			$(window).bind("scroll resize",function(e){mainobj.togglecontrol();});
+		});
+	}
+};
+scrolltotop.init();
